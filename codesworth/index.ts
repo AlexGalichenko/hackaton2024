19d512c93df0ca2df560b28dc1b6fda18cef2303
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-05-03 17:15:47 +0300

    added git fetch

--- codesworth/index.ts
+++ codesworth/index.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'node:child_process';
 
 export function getChangedFiles(targetBranch: string) {
+    execSync(`git fetch`);
     return execSync(`git --no-pager diff ${targetBranch} ':(exclude)package-lock.json'`)
         .toString()
 }
