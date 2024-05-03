0743ba05b303f5abfa4c4b09c1406832474bf6b4
Alexander Galichenko <alexandr.galichenko@gmail.com>
2024-05-03 23:02:08 +0300

    reactivated ui tests (#12)
    * reactivated ui tests

--- tests/productCard.spec.ts
+++ tests/productCard.spec.ts
@@ -10,14 +10,14 @@ test.describe('Product Card Feature', () => {
   test('Verify user able to open product card', async ({ productsPage, productPage }) => {
     const expectedTitle = await productsPage.productCard(1).title.innerText();
     await productsPage.productCard(1).title.click();
-    await expect(await productPage.title.innerText()).toEqual(expectedTitle);
+    await expect(productPage.title).toHaveText(expectedTitle);
   });
-  
+
   test('Verify user able to add product to cart from product card page', async ({ productsPage, cartPage }) => {
     const expectedProduct = await productsPage.productCard(1).title.innerText();
     await productsPage.productCard(1).addToCart.click();
     await productsPage.cart.click();
-    await expect(await cartPage.cartItem(1).title.innerText()).toEqual(expectedProduct);
+    await expect(cartPage.cartItem(1).title).toHaveText(expectedProduct);
   });
 
   test('Verify user able to remove product from cart from product card page', async ({ page }) => {
