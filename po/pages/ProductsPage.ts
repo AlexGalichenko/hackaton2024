b84214a63a0902dbced61662ba950911413bb17a
Anton Shukanau <anton_shukanau@epam.com>
2024-05-03 22:22:40 +0300

    initial tests for Products page

--- po/pages/ProductsPage.ts
+++ po/pages/ProductsPage.ts
@@ -1,13 +1,17 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
+import { ProductCard } from "../fragments/ProductCard";
 
 export class ProductsPage extends BasePage {
-  //locators
-  readonly userNameInput: Locator = this.page.locator("#user-name");
-  readonly passwordInput: Locator = this.page.locator("#password");
-  readonly loginBtn: Locator = this.page.locator("#login-button");
+  // Fragments
+  productCard(index: number): ProductCard {
+    return new ProductCard(this.page.locator(`[data-test="inventory-item"]:nth-child(${index})`));
+  }
 
-  //methods
+  // Locators
+  readonly cart: Locator = this.page.locator('#shopping_cart_container');
+
+  // Methods
   pageUrl(): string {
     return "/inventory.html";
   }
